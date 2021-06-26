003625952a62ff6038b402cc95fa87f833213e37
Dae314 <discord@dae314>
2021-06-26 11:46:30 -1000

    Remove unnecessary console.log

--- src/shared/HeroFinder.svelte
+++ src/shared/HeroFinder.svelte
@@ -298,7 +298,6 @@
 			// message should contain a clean hero object now
 			selectedHero = returnObj.message;
 		}
-		console.log(selectedHero);
 		onSuccess(idx, pos, selectedHero, oldHeroID);
 		close();
 	}
