9dca12f02eea75cdc63064feba32cc9002eec9ba
Dae314 <discord@dae314>
2021-10-03 11:38:10 -1000

    Add support for legendary tier heroes to AscensionMenu

--- src/shared/AscensionMenu.svelte
+++ src/shared/AscensionMenu.svelte
@@ -5,27 +5,54 @@
 	export let activeItem = 0;
 	export let zIndexBase = 1;
 	export let centerMenu = false;
+	export let tier = 'ascended';
 
-	let menuItems = ['E', 'E+', 'L', 'L+', 'M', 'M+', 'A'];
-	let fullItems = ['Elite', 'Elite+', 'Legendary', 'Legendary+', 'Mythic', 'Mythic+', 'Ascended'];
-	let menuItemsStyle = [
-		'background-color: #AF3CEA; color: white; border: 2px solid #AF3CEA;',
-		'background-color: #AF3CEA; color: white; border: 2px solid #6D2691;',
-		'background-color: #F7BC19; color: white; border: 2px solid #F7BC19;',
-		'background-color: #F7BC19; color: white; border: 2px solid #E0920B;',
-		'background-color: #E60B51; color: white; border: 2px solid #E60B51;',
-		'background-color: #E60B51; color: white; border: 2px solid #A6083A;',
-		'background: linear-gradient(#91BDFF, rgba(145, 189, 255, 0)), linear-gradient(-45deg, #E196FF, rgba(225, 150, 255, 0)), linear-gradient(45deg, #B1A3FE, rgba(177, 163, 254, 0)); background-blend-mode: multiply; color: white; border: 2px solid #B289E8;',
-	];
-	let fullItemsStyle = [
-		'background-color: #AF3CEA; color: white; border: 2px solid #AF3CEA;',
-		'background-color: #AF3CEA; color: white; border: 2px solid #6D2691;',
-		'background-color: #F7BC19; color: white; border: 2px solid #F7BC19;',
-		'background-color: #F7BC19; color: white; border: 2px solid #E0920B;',
-		'background-color: #E60B51; color: white; border: 2px solid #E60B51;',
-		'background-color: #E60B51; color: white; border: 2px solid #A6083A;',
-		'background: linear-gradient(#91BDFF, rgba(145, 189, 255, 0)), linear-gradient(-45deg, #E196FF, rgba(225, 150, 255, 0)), linear-gradient(45deg, #B1A3FE, rgba(177, 163, 254, 0)); background-blend-mode: multiply; color: white; border: 2px solid #B289E8;',
-	];
+	let menuItems;
+	let fullItems;
+	let menuItemsStyle;
+	let fullItemsStyle;
+
+	if(tier === 'ascended') {
+		menuItems = ['E', 'E+', 'L', 'L+', 'M', 'M+', 'A'];
+		fullItems = ['Elite', 'Elite+', 'Legendary', 'Legendary+', 'Mythic', 'Mythic+', 'Ascended'];
+		menuItemsStyle = [
+			'background-color: #AF3CEA; color: white; border: 2px solid #AF3CEA;',
+			'background-color: #AF3CEA; color: white; border: 2px solid #6D2691;',
+			'background-color: #F7BC19; color: white; border: 2px solid #F7BC19;',
+			'background-color: #F7BC19; color: white; border: 2px solid #E0920B;',
+			'background-color: #E60B51; color: white; border: 2px solid #E60B51;',
+			'background-color: #E60B51; color: white; border: 2px solid #A6083A;',
+			'background: linear-gradient(#91BDFF, rgba(145, 189, 255, 0)), linear-gradient(-45deg, #E196FF, rgba(225, 150, 255, 0)), linear-gradient(45deg, #B1A3FE, rgba(177, 163, 254, 0)); background-blend-mode: multiply; color: white; border: 2px solid #B289E8;',
+		];
+		fullItemsStyle = [
+			'background-color: #AF3CEA; color: white; border: 2px solid #AF3CEA;',
+			'background-color: #AF3CEA; color: white; border: 2px solid #6D2691;',
+			'background-color: #F7BC19; color: white; border: 2px solid #F7BC19;',
+			'background-color: #F7BC19; color: white; border: 2px solid #E0920B;',
+			'background-color: #E60B51; color: white; border: 2px solid #E60B51;',
+			'background-color: #E60B51; color: white; border: 2px solid #A6083A;',
+			'background: linear-gradient(#91BDFF, rgba(145, 189, 255, 0)), linear-gradient(-45deg, #E196FF, rgba(225, 150, 255, 0)), linear-gradient(45deg, #B1A3FE, rgba(177, 163, 254, 0)); background-blend-mode: multiply; color: white; border: 2px solid #B289E8;',
+		];
+	} else {
+		menuItems = ['R', 'R+', 'E', 'E+', 'L', 'L+'];
+		fullItems = ['Rare', 'Rare+', 'Elite', 'Elite+', 'Legendary', 'Legendary+'];
+		menuItemsStyle = [
+			'background-color: #5EBCFA; color: white; border: 2px solid #5EBCFA;',
+			'background-color: #5EBCFA; color: white; border: 2px solid #4182AD;',
+			'background-color: #AF3CEA; color: white; border: 2px solid #AF3CEA;',
+			'background-color: #AF3CEA; color: white; border: 2px solid #6D2691;',
+			'background-color: #F7BC19; color: white; border: 2px solid #F7BC19;',
+			'background-color: #F7BC19; color: white; border: 2px solid #E0920B;',
+		];
+		fullItemsStyle = [
+			'background-color: #5EBCFA; color: white; border: 2px solid #5EBCFA;',
+			'background-color: #5EBCFA; color: white; border: 2px solid #4182AD;',
+			'background-color: #AF3CEA; color: white; border: 2px solid #AF3CEA;',
+			'background-color: #AF3CEA; color: white; border: 2px solid #6D2691;',
+			'background-color: #F7BC19; color: white; border: 2px solid #F7BC19;',
+			'background-color: #F7BC19; color: white; border: 2px solid #E0920B;',
+		];
+	}
 
 	function handleMenuItemClick(index) {
 		activeItem = index;
