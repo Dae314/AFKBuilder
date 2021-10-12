6fdda2df4051b07cfbe48d2a7cca1cfcbd05f417
Dae314 <discord@dae314>
2021-10-11 21:15:29 -1000

    Disable search when number keys or numpad keys are pressed

--- src/components/MyHeroes.svelte
+++ src/components/MyHeroes.svelte
@@ -288,9 +288,7 @@
 	function isCharacterKeyPress(event) {
 		let keycode = event.keyCode;
 		let valid = 
-			(keycode > 47 && keycode < 58)   || // number keys
 			(keycode > 64 && keycode < 91)   || // letter keys
-			(keycode > 95 && keycode < 112)  || // numpad keys
 			(keycode > 185 && keycode < 193) || // ;=,-./` (in order)
 			(keycode > 218 && keycode < 223) || // [\]' (in order)
 			(keycode === 9);										// tab
