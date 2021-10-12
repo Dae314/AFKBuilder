06432f2b1f511014c7602ca2411a8fb0206246a4
Dae314 <discord@dae314>
2021-10-11 16:49:56 -1000

    Hook up basic functionality for plus and minus buttons

--- src/shared/NumPicker.svelte
+++ src/shared/NumPicker.svelte
@@ -11,9 +11,23 @@
 	function handleChange() {
 		dispatch('change', { value: value });
 	}
+
+	function handleClick(type) {
+		switch(type) {
+			case 'minus':
+				value = value - 1 < min ? min : value - 1;
+				break;
+			case 'plus':
+				value = value + 1 > max ? max : value + 1;
+				break;
+			default:
+				break;
+		}
+		dispatch('change', { value: value });
+	}
 </script>
 
-<button type="button" class="counterButton subButton" disabled={!enabled}>
+<button type="button" class="counterButton subButton" disabled={!enabled || value <= min} on:click={() => handleClick('minus')}>
 	<span>-</span>
 </button>
 <input
@@ -24,7 +38,7 @@
 	bind:value={value}
 	disabled={!enabled}
 	on:change={handleChange}>
-<button type="button" class="counterButton subButton" disabled={!enabled}>
+<button type="button" class="counterButton subButton" disabled={!enabled || value >= max} on:click={() => handleClick('plus')}>
 	<span>+</span>
 </button>
 
