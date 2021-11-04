156dbf6244e2f873989d97006b1120108cbece10
Dae314 <discord@dae314>
2021-11-03 22:54:36 -1000

    Convert StarsInput to use star shapes instead of NumPicker

--- src/shared/StarsInput.svelte
+++ src/shared/StarsInput.svelte
@@ -1,21 +1,119 @@
 <script>
-	import NumPicker from './NumPicker.svelte';
+	import { createEventDispatcher } from 'svelte';
 
-	let min = 0;
-	let max = 5;
+	const dispatch = createEventDispatcher();
+
+	const star_array = [ 1, 2, 3, 4, 5 ]
+	let hover = -1;
 	export let value = 0;
 	export let enabled = true;
+
+	function handleStarClick(starid) {
+		if(value === starid) {
+			value = 0; // reset stars to 0 if user clicks on the exact same stars
+		} else {
+			value = starid; // set stars to whatever the user clicked on otherwise
+		}
+		dispatch('change', { value: value });
+	}
 </script>
 
 <div class="title">Stars</div>
-<NumPicker 
-	{min}
-	{max}
-	bind:value={value}
-	{enabled}
-	on:change
-/>
+
+<div class="starsContainer" on:mouseleave={() => hover = -1}>
+	{#each star_array as starid}
+		<div class="singleStarContainer" on:click={(e) => handleStarClick(starid)} on:mouseenter={() => hover = starid}>
+			<i class="star"
+				class:enabled
+				class:active={value >= starid}
+				class:hovered={hover >= starid}>
+			</i>
+		</div>
+	{/each}
+</div>
 
 <style lang="scss">
-
+	.starsContainer {
+		width: fit-content;
+	}
+	.singleStarContainer {
+		display: inline-block;
+	}
+	.star {
+		border-bottom: .7em solid lightgray;
+		border-left: .3em solid transparent;
+		border-right: .3em solid transparent;
+		cursor: pointer;
+		display: inline-block;
+		font-size: 0.7rem;
+		height: 0;
+		margin-bottom: 1.2em;
+		margin-left: .9em;
+		margin-right: .9em;
+		position: relative;
+		width: 0;
+		&:before {
+			border-bottom: .7em solid lightgray;
+			border-left: 1em solid transparent;
+			border-right: 1em solid transparent;
+			content: '';
+			display: block;
+			height: 0;
+			left: -1em;
+			position: absolute;
+			top: .6em;
+			transform: rotate(-35deg);
+			width: 0;
+		}
+		&:after {
+			border-bottom: .7em solid lightgray;
+			border-left: 1em solid transparent;
+			border-right: 1em solid transparent;
+			content: '';
+			display: block;
+			height: 0;
+			left: -1em;
+			position: absolute;
+			top: .6em;
+			transform: rotate(-35deg);
+			width: 0;
+			transform: rotate(35deg);
+		}
+		&:active {
+			border-bottom-color: var(--legendColor);
+			&:before {
+				border-bottom-color: var(--legendColor);
+			}
+			&:after {
+				border-bottom-color: var(--legendColor);
+			}
+		}
+	}
+	.star.enabled {
+		border-bottom: .7em solid gray;
+		&:before {
+			border-bottom: .7em solid gray;
+		}
+		&:after {
+			border-bottom: .7em solid gray;
+		}
+	}
+	.star.hovered {
+		border-bottom-color: lightgoldenrodyellow;
+		&:before {
+			border-bottom-color: lightgoldenrodyellow;
+		}
+		&:after {
+			border-bottom-color: lightgoldenrodyellow;
+		}
+	}
+	.star.active {
+		border-bottom-color: var(--legendColor);
+		&:before {
+			border-bottom-color: var(--legendColor);
+		}
+		&:after {
+			border-bottom-color: var(--legendColor);
+		}
+	}
 </style>
