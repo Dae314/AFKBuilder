0e222b739808ac9878b1f2fb34fb385e6468e8b1
Dae314 <discord@dae314>
2022-02-09 16:12:27 -1000

    Move LoadingSpinner into its own component

--- src/shared/LoadingPage.svelte
+++ src/shared/LoadingPage.svelte
@@ -1,9 +1,9 @@
 <script>
-	// https://loading.io/css/
+	import LoadingSpinner from './LoadingSpinner.svelte';
 </script>
 
 <div class="loadingContainer">
-	<div class="lds-dual-ring"></div>
+	<LoadingSpinner />
 </div>
 
 <style lang="scss">
@@ -19,29 +19,5 @@
 		top: 0px;
 		width: 100%;
 		z-index: 10;
-		.lds-dual-ring {
-			display: inline-block;
-			height: 80px;
-			width: 80px;
-			&:after {
-				content: '';
-				display: block;
-				width: 64px;
-				height: 64px;
-				margin: 8px;
-				border-radius: 50%;
-				border: 6px solid #fff;
-				border-color: #fff transparent #fff transparent;
-				animation: lds-dual-ring 1.2s linear infinite;
-			}
-		}
-	}
-	@keyframes lds-dual-ring {
-		0% {
-			transform: rotate(0deg);
-		}
-		100% {
-			transform: rotate(360deg);
-		}
 	}
 </style>
---
+++ src/shared/LoadingSpinner.svelte
@@ -0,0 +1,32 @@
+<script>
+	// https://loading.io/css/
+</script>
+
+<div class="lds-dual-ring"></div>
+
+<style lang="scss">
+	.lds-dual-ring {
+		display: inline-block;
+		height: 80px;
+		width: 80px;
+		&:after {
+			content: '';
+			display: block;
+			width: 64px;
+			height: 64px;
+			margin: 8px;
+			border-radius: 50%;
+			border: 6px solid #fff;
+			border-color: #fff transparent #fff transparent;
+			animation: lds-dual-ring 1.2s linear infinite;
+		}
+	}
+	@keyframes lds-dual-ring {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
