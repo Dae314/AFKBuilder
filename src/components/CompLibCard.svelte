2729adde4772a0ff5b92f4ad54da5908c1bfe3a8
Dae314 <discord@dae314>
2022-02-13 10:26:59 -1000

    Add comments for ToString functions

--- src/components/CompLibCard.svelte
+++ src/components/CompLibCard.svelte
@@ -50,24 +50,28 @@
 
 	function msToString(time) {
 		// expect time to be number of milliseconds
-		// time is less than a minute
+		// return seconds if time is < 1 minute
 		if(time < 6e4) return `a few seconds`;
-		// time is less than an hour
+		// return minutes if time is < 1 hour
 		if(time < 3.6e6) return `${Math.floor(time/6e4)} min`;
-		// time is less than a day
+		// return hours if time is < 1 day
 		if(time < 8.64e7) return `${Math.floor(time/3.6e6)} hrs`;
-		// time is less than a month
+		// return days if time is < 1 month
 		if(time < 2.592e9) return `${Math.floor(time/8.64e7)} days`;
-		// time is less than a year
+		// return months if time is < 1 year
 		if(time < 3.156e10) return `${Math.floor(time/2.592e9)} mo`;
-		// time is greater than a year
+		// return years if time is >= 1 year
 		return `${Math.floor(time/3.156e10)} yrs`;
 	}
 
 	function votesToString(votes) {
+		// return votes if votes is < 1k
 		if(votes < 1e3) return votes;
+		// return #.#k if votes is < 1m
 		if(votes < 1e6) return `${Math.round((votes/1e3) * 10)/10}k`;
+		// return #.#m if votes is < 1b
 		if(votes < 1e9) return `${Math.round(votes/1e6 * 10)/10}m`;
+		// return 'a lot' if votes is >= 1b
 		return 'a lot';
 	}
 
