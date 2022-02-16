035dcf0a62d60bf6c0e5f1142d14f64f4c88e85a
Dae314 <discord@dae314>
2022-02-15 17:16:19 -1000

    Move msToString and votesToString to a utilities file and apply initial layout and styling to CompLibDetail

--- src/components/CompLibCard.svelte
+++ src/components/CompLibCard.svelte
@@ -3,6 +3,7 @@
 	import HeroData from '../stores/HeroData.js';
 	import AppData from '../stores/AppData.js';
 	import {validateJWT, toggleSave, toggleUpvote, toggleDownvote} from '../rest/RESTFunctions.svelte';
+	import {msToString, votesToString} from '../utilities/Utilities.svelte';
 	
 	export let comp = {};
 	/* expect comp to be of the format:
@@ -50,33 +51,6 @@
 	const dispatch = createEventDispatcher();
 	let showError = false;
 	let errorConf = {};
-
-	function msToString(time) {
-		// expect time to be number of milliseconds
-		// return seconds if time is < 1 minute
-		if(time < 6e4) return `a few seconds ago`;
-		// return minutes if time is < 1 hour
-		if(time < 3.6e6) return `${Math.floor(time/6e4)} min ago`;
-		// return hours if time is < 1 day
-		if(time < 8.64e7) return `${Math.floor(time/3.6e6)} hours ago`;
-		// return days if time is < 1 month
-		if(time < 2.592e9) return `${Math.floor(time/8.64e7)} days ago`;
-		// return months if time is < 1 year
-		if(time < 3.156e10) return `${Math.floor(time/2.592e9)} months ago`;
-		// return years if time is >= 1 year
-		return `${Math.floor(time/3.156e10)} years ago`;
-	}
-
-	function votesToString(votes) {
-		// return votes if votes is < 1k
-		if(votes < 1e3) return votes;
-		// return #.#k if votes is < 1m
-		if(votes < 1e6) return `${Math.round((votes/1e3) * 10)/10}k`;
-		// return #.#m if votes is < 1b
-		if(votes < 1e9) return `${Math.round(votes/1e6 * 10)/10}m`;
-		// return 'a lot' if votes is >= 1b
-		return 'a lot';
-	}
 
 	function handleCompCardClick() {
 		// navigate to the comp detail view
--- src/components/CompLibDetail.svelte
+++ src/components/CompLibDetail.svelte
@@ -1,11 +1,14 @@
 <script>
+	import { query } from 'svelte-apollo';
+	import JSONURL from 'json-url';
 	import AppData from '../stores/AppData.js';
+	import HeroData from '../stores/HeroData.js';
 	import LoadingPage from '../shared/LoadingPage.svelte';
 	import ErrorDisplay from './ErrorDisplay.svelte';
 	import { gql_GET_COMP } from '../gql/queries.svelte';
 	import { getCompAuthor } from '../rest/RESTFunctions.svelte';
-	import { query } from 'svelte-apollo';
-	import JSONURL from 'json-url';
+	import { msToString } from '../utilities/Utilities.svelte';
+	
 
 	export let params = {};
 	let svrComp;
@@ -14,16 +17,20 @@
 	let showErrorDisplay = false;
 	let errorDisplayConf = {};
 	const jsurl = JSONURL('lzma'); // json-url compressor
-
+	let populatePromise;
 	const compQuery = query(gql_GET_COMP, {
 		variables: { uuid: params.uuid }
 	});
 
+	// redo the query comp if the params change
+	$: compQuery.refetch({ uuid: params.uuid });
+	// reload the comp data if the query is rerun
 	$: if(!$compQuery.loading) {
 		svrComp = $compQuery.data.comps.data[0];
-		populateAuthor();
+		populatePromise = Promise.all([populateAuthor(), populateComp()]);
 	}
-	$: compQuery.refetch({ uuid: params.uuid });
+	$: avatarHero = author ? $HeroData.find(e => e.id === author.avatar) : '';
+	$: age = comp ? Date.now() - comp.lastUpdate.getTime() : '';
 
 	function reload() {
 		compQuery.refetch({ uuid: params.uuid });
@@ -42,13 +49,14 @@
 		} else {
 			author = response.data.author;
 		}
+		console.log(author);
 	}
 
-	async function unpackComp(comp_string) {
+	async function populateComp() {
 		// unpack and decompress data
 		let data;
 		try {
-			const json = await jsurl.decompress(comp_string);
+			const json = await jsurl.decompress(svrComp.attributes.comp_string);
 			data = JSON.parse(json);
 		} catch(err) {
 			errorDisplayConf = {
@@ -61,6 +69,7 @@
 			showErrorDisplay = true;
 			return;
 		}
+		if('lastUpdate' in data) data.lastUpdate = new Date(data.lastUpdate);
 
 		// run consistency checks on data
 		const returnObj = await validateComp(data);
@@ -76,7 +85,12 @@
 		} else {
 			// validation successful, import the comp
 			comp = returnObj.message;
+			console.log(comp);
 		}
+	}
+
+	function handleAuthorClick() {
+		console.log('author clicked');
 	}
 </script>
 
@@ -99,7 +113,7 @@
 			showHomeButton={false}
 		/>
 	{:else}
-		{#await Promise.all([populateAuthor(), unpackComp(svrComp.comp_string)])}
+		{#await populatePromise}
 			<LoadingPage />
 		{:then _}
 			{#if showErrorDisplay}
@@ -111,9 +125,263 @@
 				/>
 			{:else}
 				<div class="compLibDetailContainer">
-					{svrComp.attributes.name}
-					{author.username}
-					{comp.heroes}
+					<div class="compLibDetailHead">
+						<div class="titleContainer">
+							<div class="authorAgeContainer">
+								<button type="button" class="authorButton" on:click={handleAuthorClick}>
+									<img class="avatar" src="{avatarHero.portrait}" alt="{avatarHero.name}" draggable="false">
+									<div class="username">{author.username}</div>
+								</button>
+								<div class="ageContainer">
+									<span class="age">{msToString(age)}</span>
+								</div>
+							</div>
+							<h3 class="compTitle">{comp.name}</h3>
+						</div>
+						<div class="buttonContainer">
+						</div>
+					</div>
+					<!--
+					<div class="tagsArea">
+						<div class="tagDisplay">
+							{#each sortedCompList[$AppData.selectedComp].tags as tag}
+								<div class="tag">
+									<span class="tagText">{tag}</span>
+								</div>
+							{/each}
+						</div>
+					</div>
+					<div class="compDetailBody">
+						<div class="lastUpdate">
+							<span>Updated: {`${months[sortedCompList[$AppData.selectedComp].lastUpdate.getMonth()]} ${sortedCompList[$AppData.selectedComp].lastUpdate.getDate()}, ${sortedCompList[$AppData.selectedComp].lastUpdate.getFullYear()}`}</span>
+						</div>
+						<div class="bodyArea1">
+							<div class="lineExamples">
+								<div class="lineSwitcher">
+									{#each sortedCompList[$AppData.selectedComp].lines as line, i}
+									<button type="button" class="lineSwitchButton" class:active={selectedLine === i} on:click={() => selectedLine = i}>{line.name}</button>
+									{/each}
+								</div>
+								<div class="lineDisplay">
+									{#if sortedCompList[$AppData.selectedComp].lines.length > 0}
+										<div class="lineTitle"><span>{sortedCompList[$AppData.selectedComp].lines[selectedLine].name}</span></div>
+									{/if}
+									<div class="lineMembers">
+										<div class="detailBackline">
+											{#if sortedCompList[$AppData.selectedComp].lines.length > 0}
+												{#each sortedCompList[$AppData.selectedComp].lines[selectedLine].heroes as hero, i}
+													{#if i >= 2}
+														{#if $HeroData.some(e => e.id === hero)}
+															<div class="detailImgContainer">
+																<button type="button" class="heroButton"><img draggable="false" on:click={() => handleHeroClick(hero)} class="lineImg" class:claimed={$AppData.MH.List[hero].claimed} src={$HeroData.find(e => e.id === hero).portrait} alt={$HeroData.find(e => e.id === hero).name}></button>
+																<span class="coreMark" class:visible={sortedCompList[$AppData.selectedComp].heroes[hero].core}></span>
+																<div class="ascMark">
+																	{#if $HeroData.find(e => e.id === hero).tier === 'ascended'}
+																		{#if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 6}
+																			<img src="./img/markers/ascended.png" alt="ascended">
+																		{:else if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 4}
+																			<img src="./img/markers/mythic.png" alt="mythic">
+																		{:else if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 2}
+																			<img src="./img/markers/legendary.png" alt="legendary">
+																		{:else}
+																			<img src="./img/markers/elite.png" alt="elite">
+																		{/if}
+																	{:else}
+																		{#if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 4}
+																			<img src="./img/markers/legendary.png" alt="legendary">
+																		{:else if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 2}
+																			<img src="./img/markers/elite.png" alt="elite">
+																		{:else}
+																			<img src="./img/markers/rare.png" alt="rare">
+																		{/if}
+																	{/if}
+																	{#if sortedCompList[$AppData.selectedComp].heroes[hero].si >= 30}
+																		<img src="./img/markers/si30.png" alt="si30">
+																	{:else if sortedCompList[$AppData.selectedComp].heroes[hero].si >= 20}
+																		<img src="./img/markers/si20.png" alt="si20">
+																	{:else if sortedCompList[$AppData.selectedComp].heroes[hero].si >= 10}
+																		<img src="./img/markers/si10.png" alt="si10">
+																	{:else}
+																		<img src="./img/markers/si0.png" alt="si0">
+																	{/if}
+																	{#if sortedCompList[$AppData.selectedComp].heroes[hero].furn >= 9}
+																		<img class:moveup={sortedCompList[$AppData.selectedComp].heroes[hero].si < 10} src="./img/markers/9f.png" alt="9f">
+																	{:else if sortedCompList[$AppData.selectedComp].heroes[hero].furn >= 3}
+																		<img class:moveup={sortedCompList[$AppData.selectedComp].heroes[hero].si < 10} src="./img/markers/3f.png" alt="3f">
+																	{/if}
+																</div>
+															</div>
+															<button type="button" class="heroNameButton"><span on:click={() => handleHeroClick(hero)}>{$HeroData.find(e => e.id === hero).name}</span></button>
+														{:else}
+															<i class="emptyLineSlot"></i>
+														{/if}
+													{/if}
+												{/each}
+											{/if}
+										</div>
+										<div class="detailFrontline">
+											{#if sortedCompList[$AppData.selectedComp].lines.length > 0}
+												{#each sortedCompList[$AppData.selectedComp].lines[selectedLine].heroes as hero, i}
+													{#if i < 2}
+														{#if $HeroData.some(e => e.id === hero)}
+															<div class="detailImgContainer">
+																<button type="button" class="heroButton"><img draggable="false" on:click={() => handleHeroClick(hero)} class="lineImg" class:claimed={$AppData.MH.List[hero].claimed} src={$HeroData.find(e => e.id === hero).portrait} alt={$HeroData.find(e => e.id === hero).name}></button>
+																<span class="coreMark" class:visible={sortedCompList[$AppData.selectedComp].heroes[hero].core}></span>
+																<div class="ascMark">
+																	{#if $HeroData.find(e => e.id === hero).tier === 'ascended'}
+																		{#if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 6}
+																			<img src="./img/markers/ascended.png" alt="ascended">
+																		{:else if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 4}
+																			<img src="./img/markers/mythic.png" alt="mythic">
+																		{:else if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 2}
+																			<img src="./img/markers/legendary.png" alt="legendary">
+																		{:else}
+																			<img src="./img/markers/elite.png" alt="elite">
+																		{/if}
+																	{:else}
+																		{#if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 4}
+																			<img src="./img/markers/legendary.png" alt="legendary">
+																		{:else if sortedCompList[$AppData.selectedComp].heroes[hero].ascendLv >= 2}
+																			<img src="./img/markers/elite.png" alt="elite">
+																		{:else}
+																			<img src="./img/markers/rare.png" alt="rare">
+																		{/if}
+																	{/if}
+																	{#if sortedCompList[$AppData.selectedComp].heroes[hero].si >= 30}
+																		<img src="./img/markers/si30.png" alt="si30">
+																	{:else if sortedCompList[$AppData.selectedComp].heroes[hero].si >= 20}
+																		<img src="./img/markers/si20.png" alt="si20">
+																	{:else if sortedCompList[$AppData.selectedComp].heroes[hero].si >= 10}
+																		<img src="./img/markers/si10.png" alt="si10">
+																	{:else}
+																		<img src="./img/markers/si0.png" alt="si0">
+																	{/if}
+																	{#if sortedCompList[$AppData.selectedComp].heroes[hero].furn >= 9}
+																		<img class:moveup={sortedCompList[$AppData.selectedComp].heroes[hero].si < 10} src="./img/markers/9f.png" alt="9f">
+																	{:else if sortedCompList[$AppData.selectedComp].heroes[hero].furn >= 3}
+																		<img class:moveup={sortedCompList[$AppData.selectedComp].heroes[hero].si < 10} src="./img/markers/3f.png" alt="3f">
+																	{/if}
+																</div>
+															</div>
+															<button type="button" class="heroNameButton"><span on:click={() => handleHeroClick(hero)}>{$HeroData.find(e => e.id === hero).name}</span></button>
+														{:else}
+															<i class="emptyLineSlot"></i>
+														{/if}
+													{/if}
+												{/each}
+											{/if}
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="description">
+								<div class="mobileExpanderTitle">
+									<button type="button" class="expanderButton" on:click={() => openDesc = !openDesc}><i class="expanderArrow {openDesc ? 'down' : 'right' }"></i><span>Description</span></button>
+								</div>
+								<div class="mobileExpander descSection" class:open={openDesc}>
+									<span class="descText">{@html renderMarkdown(sortedCompList[$AppData.selectedComp].desc)}</span>
+								</div>
+							</div>
+						</div>
+						<div class="bodyArea2">
+							<div class="heroDetails" id="heroDetailSection">
+								<div class="mobileExpanderTitle">
+									<button type="button" class="expanderButton" on:click={() => openHero = !openHero}><i class="expanderArrow {openHero ? 'down' : 'right' }"></i><span>Hero Info</span></button>
+								</div>
+								<div class="mobileExpander selectHeroSection" class:open={openHero}>
+									{#if selectedHero !== ''}
+										<div class="selectedHero" in:fade="{{duration: 200}}">
+											<div class="upperSelectCard">
+												<div class="siFurnBoxContainer">
+													<SIFurnEngBox type='si' num={sortedCompList[$AppData.selectedComp].heroes[selectedHero].si} maxWidth='50px' fontSize='1.2rem' />
+												</div>
+												<div class="selectPortraitArea">
+													<div class="portraitContainer" on:click={() => handleHeroDetailClick(selectedHero)}>
+														<img draggable="false" class="selectHeroPortrait" class:claimed={$AppData.MH.List[selectedHero].claimed} src="{$HeroData.find(e => e.id === selectedHero).portrait}" alt="{selectedHero}">
+														<span class="coreMark" class:visible={sortedCompList[$AppData.selectedComp].heroes[selectedHero].core}></span>
+													</div>
+													<p>{$HeroData.find(e => e.id === selectedHero).name}</p>
+													<div>
+														<StarsInput
+															value={sortedCompList[$AppData.selectedComp].heroes[selectedHero].stars}
+															enabled={sortedCompList[$AppData.selectedComp].heroes[selectedHero].ascendLv === 6}
+															engraving={sortedCompList[$AppData.selectedComp].heroes[selectedHero].engraving}
+															displayOnly={true} />
+													</div>
+												</div>
+												<div class="siFurnBoxContainer">
+													<SIFurnEngBox type='furn' num={sortedCompList[$AppData.selectedComp].heroes[selectedHero].furn} maxWidth='50px' fontSize='1.2rem' />
+												</div>
+											</div>
+											<div class="lowerSelectCard">
+												<div class="ascendBoxContainer">
+													<AscendBox
+														ascendLv="{sortedCompList[$AppData.selectedComp].heroes[selectedHero].ascendLv}"
+														tier={$HeroData.find(e => e.id === selectedHero).tier}
+													/>
+												</div>
+												{#if sortedCompList[$AppData.selectedComp].heroes[selectedHero].stars > 0}
+													<div class="engraveBoxContainer">
+														<SIFurnEngBox type='engraving' num={sortedCompList[$AppData.selectedComp].heroes[selectedHero].engraving} maxWidth='50px' fontSize='1.2rem' />
+													</div>
+												{/if}
+												{#if sortedCompList[$AppData.selectedComp].heroes[selectedHero].notes.length > 0}
+													<div class="heroNotesArea">
+														<div class="heroNotes">
+															<span>{sortedCompList[$AppData.selectedComp].heroes[selectedHero].notes}</span>
+														</div>
+													</div>
+												{/if}
+												{#if sortedCompList[$AppData.selectedComp].heroes[selectedHero].artifacts.primary.length > 0 || sortedCompList[$AppData.selectedComp].heroes[selectedHero].artifacts.secondary.length > 0 || sortedCompList[$AppData.selectedComp].heroes[selectedHero].artifacts.situational.length > 0}
+													<div class="artifactsContainer">
+														<h5>Artifacts</h5>
+														<div class="artifactLine priArtifactLine">
+															<h6>Primary</h6>
+															<div class="artifactArea">
+																{#each sortedCompList[$AppData.selectedComp].heroes[selectedHero].artifacts.primary as artifact}
+																	<button type="button" on:click={() => openArtifactDetail(artifact)} class="artifactImgContainer">
+																		<img draggable="false" src="{$Artifacts[artifact].image}" alt="{$Artifacts[artifact].name}">
+																		<p>{$Artifacts[artifact].name}</p>
+																	</button>
+																{/each}
+															</div>
+														</div>
+														{#if sortedCompList[$AppData.selectedComp].heroes[selectedHero].artifacts.secondary.length > 0}
+															<div class="artifactLine secArtifactLine">
+																<h6>Secondary</h6>
+																<div class="artifactArea">
+																	{#each sortedCompList[$AppData.selectedComp].heroes[selectedHero].artifacts.secondary as artifact}
+																		<button type="button" on:click={() => openArtifactDetail(artifact)} class="artifactImgContainer">
+																			<img draggable="false" src="{$Artifacts[artifact].image}" alt="{$Artifacts[artifact].name}">
+																			<p>{$Artifacts[artifact].name}</p>
+																		</button>
+																	{/each}
+																</div>
+															</div>
+														{/if}
+														{#if sortedCompList[$AppData.selectedComp].heroes[selectedHero].artifacts.situational.length > 0}
+															<div class="artifactLine sitArtifactLine">
+																<h6>Situational</h6>
+																<div class="artifactArea">
+																	{#each sortedCompList[$AppData.selectedComp].heroes[selectedHero].artifacts.situational as artifact}
+																		<button type="button" on:click={() => openArtifactDetail(artifact)} class="artifactImgContainer">
+																			<img draggable="false" src="{$Artifacts[artifact].image}" alt="{$Artifacts[artifact].name}">
+																			<p>{$Artifacts[artifact].name}</p>
+																		</button>
+																	{/each}
+																</div>
+															</div>
+														{/if}
+													</div>
+												{/if}
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					-->
 				</div>
 			{/if}
 		{/await}
@@ -124,5 +392,30 @@
 	.compLibDetailContainer {
 		height: calc(100vh - var(--headerHeight));
 		width: 100%;
+	}
+	.compLibDetailHead {
+		display: flex;
+		flex-direction: column;
+		.authorAgeContainer {
+			display: flex;
+			flex-direction: column;
+			.authorButton {
+				align-items: center;
+				background: transparent;
+				border: none;
+				cursor: pointer;
+				display: flex;
+				outline: none;
+				.avatar {
+					border-radius: 50%;
+					max-width: 40px;
+				}
+				.username {
+					font-size: 1rem;
+					margin-left: 10px;
+					user-select: none;
+				}
+			}
+		}
 	}
 </style>
