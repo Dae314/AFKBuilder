6cf721c97a053fd9556dfaa0457fc8b540a31dae
Dae314 <discord@dae314>
2022-03-09 16:41:00 -1000

    Remove artifical delay line

--- src/components/User.svelte
+++ src/components/User.svelte
@@ -25,7 +25,6 @@
 	});
 
 	async function populateAuthorData() {
-		// await new Promise(resolve => setTimeout(resolve, 3000));
 		const response = await getAuthorDetails(username);
 		if(response.status !== 200) {
 			errorDisplayConf = {
