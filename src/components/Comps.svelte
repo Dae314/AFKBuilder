50dcee41153e436e21e2d72e17fc77da1bab52fe
Dae314 <discord@dae314>
2022-03-13 15:18:37 -1000

    Convert to using icon image for hide/unhide

--- src/components/Comps.svelte
+++ src/components/Comps.svelte
@@ -862,10 +862,10 @@
 							class="editDelButton hideButton"
 							class:hidden={sortedCompList[$AppData.selectedComp].hidden}
 							on:click={() => handleHideButtonClick($AppData.selectedComp)}>
-							<i class:gg-eye={!sortedCompList[$AppData.selectedComp].hidden} class:gg-eye-alt={sortedCompList[$AppData.selectedComp].hidden}></i>
+							<img draggable="false" src={sortedCompList[$AppData.selectedComp].hidden ? './img/utility/view_white.png' : './img/utility/hidden_white.png'} alt={sortedCompList[$AppData.selectedComp].hidden ? 'Unhide' : 'Hide'}>
 							<span>{sortedCompList[$AppData.selectedComp].hidden ? 'Unhide' : 'Hide'}</span>
 						</button>
-						<!-- eye icons by https://css.gg -->
+						<!-- eye icons by https://uxwing.com/ -->
 						<button
 							type="button"
 							class="editDelButton copyButton"
@@ -1621,84 +1621,6 @@
 			&.deleteButton {
 				background-color: var(--appDelColor);
 				border: 3px solid var(--appDelColor);
-			}
-			&.hideButton {
-				i {
-					margin-right: 5px;
-				}
-				.gg-eye {
-					position: relative;
-					display: block;
-					transform: scale(var(--ggs,1));
-					width: 24px;
-					height: 18px;
-					border-bottom-right-radius: 100px;
-					border-bottom-left-radius: 100px;
-					overflow: hidden;
-					box-sizing: border-box;
-					&::after,
-					&::before {
-						content: "";
-						display: block;
-						border-radius: 100px;
-						position: absolute;
-						box-sizing: border-box;
-					}
-					&::after {
-						top: 2px;
-						box-shadow:
-							inset 0 -8px 0 2px,
-							inset 0 0 0 2px;
-						width: 24px;
-						height: 24px;
-					}
-					&::before {
-						width: 8px;
-						height: 8px;
-						border: 2px solid;
-						bottom: 4px;
-						left: 8px;
-					}
-				}
-				.gg-eye-alt {
-					position: relative;
-					display: block;
-					transform: scale(var(--ggs,1));
-					width: 24px;
-					height: 18px;
-					border-bottom-right-radius: 100px;
-					border-bottom-left-radius: 100px;
-					overflow: hidden;
-					box-sizing: border-box;
-					&::after,
-					&::before {
-						content: "";
-						display: block;
-						border-radius: 100px;
-						position: absolute;
-						box-sizing: border-box;
-					}
-					&::after {
-						top: 2px;
-						box-shadow:
-								inset 0 -8px 0 2px,
-								inset 0 0 0 2px;
-						width: 24px;
-						height: 24px
-					}
-					&::before {
-						width: 8px;
-						height: 8px;
-						border: 2px solid transparent;
-						box-shadow:
-								inset 0 0 0 6px,
-								0 0 0 4px,
-								6px 0 0 0,
-								-6px 0 0 0 ;
-						bottom: 4px;
-						left: 8px
-					}
-				}
 			}
 			&:disabled {
 				background-color: var(--appColorDisabled);
