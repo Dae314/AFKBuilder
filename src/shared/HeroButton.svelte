9e45ffe433c4def4c369c2785d868554b4f71736
Dae314 <discord@dae314>
2022-03-29 17:35:01 -1000

    Re-organize HeroButton HTML and styling

--- src/shared/HeroButton.svelte
+++ src/shared/HeroButton.svelte
@@ -15,9 +15,15 @@
 </script>
 
 <div class="heroButtonContainer">
-	<div class="detailImgContainer">
-		<button type="button" class="heroButton">
-			<img draggable="false" on:click={() => handleHeroClick(hero)} class="lineImg" class:claimed={$AppData.MH.List[hero].claimed} src={$HeroData.find(e => e.id === hero).portrait} alt={$HeroData.find(e => e.id === hero).name}>
+	<div class="portraitArea">
+		<button type="button" class="heroButton" on:click={() => handleHeroClick(hero)}>
+			<img
+				draggable="false"
+				class="heroImage"
+				class:claimed={$AppData.MH.List[hero].claimed}
+				src={$HeroData.find(e => e.id === hero).portrait}
+				alt={$HeroData.find(e => e.id === hero).name}
+			>
 		</button>
 		<span class="coreMark" class:visible={heroDetails.core}></span>
 		<div class="ascMark">
@@ -56,75 +62,90 @@
 			{/if}
 		</div>
 	</div>
-	<button type="button" class="heroNameButton"><span on:click={() => handleHeroClick(hero)}>{$HeroData.find(e => e.id === hero).name}</span></button>
+	<div class="nameArea">
+		<button type="button" class="heroNameButton">
+			<span on:click={() => handleHeroClick(hero)}>{$HeroData.find(e => e.id === hero).name}</span>
+		</button>
+	</div>
 </div>
 
 <style lang="scss">
-	.heroButton {
-		background: transparent;
-		border: none;
-		cursor: pointer;
-		margin: 0;
-		outline: none;
-		padding: 0;
-	}
-	.heroNameButton {
-		background: transparent;
-		border: none;
-		cursor: pointer;
-		margin: 0;
-		outline: none;
-		padding: 0;
-	}
-	.detailImgContainer {
+	.portraitArea {
 		position: relative;
-	}
-	.lineImg {
-		border-radius: 50%;
-		cursor: pointer;
-		margin: 5px;
-		max-width: 70px;
-		&.claimed {
-			border: 5px solid var(--appColorPrimary);
+		.heroButton {
+			background: transparent;
+			border: none;
+			border-radius: 50%;
+			cursor: pointer;
+			height: 70px;
+			margin: 0;
+			outline: none;
+			padding: 0;
+			width: 70px;
+			.heroImage {
+				border-radius: 50%;
+				height: 70px;
+				max-width: 70px;
+				width: 70px;
+				&.claimed {
+					border: 5px solid var(--appColorPrimary);
+				}
+			}
 		}
-	}
-	.coreMark {
-		background-color: var(--legendColor);
-		border: 3px solid var(--appBGColor);
-		border-radius: 50%;
-		bottom: 5px;
-		display: none;
-		height: 22px;
-		position: absolute;
-		right: 4px;
-		visibility: hidden;
-		width: 22px;
-		&.visible {
-			display: inline-block;
-			pointer-events: none;
-			visibility: visible;
-		}
-	}
-	.ascMark {
-		left: -6px;
-		position: absolute;
-		top: 3px;
-		img {
-			left: 0;
-			max-width: 35px;
-			pointer-events: none;
+		.coreMark {
+			background-color: var(--legendColor);
+			border: 3px solid var(--appBGColor);
+			border-radius: 50%;
+			bottom: 0px;
+			display: none;
+			height: 22px;
 			position: absolute;
-			top: 0;
+			right: 0px;
+			visibility: hidden;
+			width: 22px;
+			&.visible {
+				display: inline-block;
+				pointer-events: none;
+				visibility: visible;
+			}
 		}
-		img.moveup {
-			top: -3.5px;
+		.ascMark {
+			left: -11px;
+			position: absolute;
+			top: -3px;
+			img {
+				left: 0;
+				max-width: 35px;
+				pointer-events: none;
+				position: absolute;
+				top: 0;
+			}
+			img.moveup {
+				top: -3.5px;
+			}
+		}
+	}
+	.nameArea {
+		display: flex;
+		width: 100%;
+		.heroNameButton {
+			background: transparent;
+			border: none;
+			cursor: pointer;
+			margin: 0 auto;
+			outline: none;
+			padding: 0;
 		}
 	}
 	@media only screen and (min-width: 767px) {
-		.lineImg {
-			transition: transform 0.2s cubic-bezier(0.4, 0, 0.2, 0);
-			&:hover {
-				transform: scale(1.1);
+		.portraitArea {
+			.heroButton {
+				.heroImage {
+					transition: transform 0.2s cubic-bezier(0.4, 0, 0.2, 0);
+					&:hover {
+						transform: scale(1.1);
+					}
+				}
 			}
 		}
 	}
